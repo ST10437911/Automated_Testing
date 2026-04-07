2f11a70ae06325f8fe86e1b10e472c7b3f9579f3
Ziyanda Mpungose <ST10437911@vcconnect.edu.za>
2026-04-07 08:44:32 +0200

    Commit01: Adding Projects Files

---
+++ Automated_Testing/src/main/java/com/mycompany/automated_testing/Automated_Testing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_testing;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Automated_Testing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
